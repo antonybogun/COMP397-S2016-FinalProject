2b4c2ec7c1325156ba8544a509a627a8422b4fba
leontibrechko <leontibrechko@gmail.com>
2016-08-01 12:08:11 -0400

    - Created Level abstract class to manage current level in the Play scene - Implemented 1st level using new abstraction - Added Level class to config module with public variables to manage instantiation of levels

---
+++ Scripts/objects/level.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Anton Bogun
+ * @author Liavontsi Brechka
+ * @studentID 300863440
+ * @studentID 300800345
+ * @date August 1, 2016
+ * @description COMP397 - Web Game Programming - Final Project - The JavaScript Arcade Game
+ * @version 0.1 - Initial version of Flying Dead
+ */
+
+module objects {
+    export abstract class Level extends createjs.Container {
+        constructor() {
+            super();
+
+            this.InitializeLevel();
+        }
+
+        public abstract InitializeLevel():void;
+
+        public abstract UpdateLevel():void;
+    }
+}
